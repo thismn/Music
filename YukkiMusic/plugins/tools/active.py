ff4ae8bdc81cbbf4043249a0efd0ace2b7ec8bc2
Mon <91760182+thismn@users.noreply.github.com>
2022-06-11 01:20:13 +0700

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -15,14 +15,10 @@ from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
-from YukkiMusic.utils.database.mongodatabase import (
-    is_served_chat, get_served_chats)
-
 
 # Commands
 ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
 ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
-ACTIVECH_COMMAND = get_command("ACTIVECH_COMMAND")
 
 @app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
 async def activevc(_, message: Message):
@@ -76,32 +72,5 @@ async def activevi_(_, message: Message):
     else:
         await mystic.edit_text(
             f"**Active Video Calls:-**\n\n{text}",
-            disable_web_page_preview=True,
-        )
-
-@app.on_message(filters.command(ACTIVECH_COMMAND) & SUDOERS)
-async def activech(_, message: Message):
-    mystic = await message.reply_text(
-        "Getting chats.. Please hold"
-    )
-    served_chats = await get_served_chats()
-    text = ""
-    j = 0
-    for x in served_chats:
-        try:
-            title = (await app.get_chat(x)).title
-        except Exception:
-            title = "Private Group"
-        if (await app.get_chat(x)).username:
-            user = (await app.get_chat(x)).username
-            text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})\n➥ : [`{x}`]\n\n"
-        else:
-            text += f"<b>{j + 1}. {title}</b>\n➥ : [`{x}`]\n\n"
-        j += 1
-    if not text:
-        await mystic.edit_text("No Active Chats")
-    else:
-        await mystic.edit_text(
-            f"**Active Chats:-**\n\n{text}",
             disable_web_page_preview=True,
         )
