eca968ad9e7ffa511cf94bb93bb0665c3b08780c
Mon <91760182+thismn@users.noreply.github.com>
2022-06-20 17:28:45 +0700

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -192,7 +192,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await gen_thumb(vidid)
-            button = stream_markup(_, vidid)
+            button = stream_markup(_, vidid, chat_id)
             await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -243,7 +243,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -297,7 +297,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL
@@ -355,7 +355,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await gen_thumb(vidid)
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -405,7 +405,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
