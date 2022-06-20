8a408297df883dcd3461b7dad052431271579e81
Mon <91760182+thismn@users.noreply.github.com>
2022-06-20 17:36:12 +0700

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -123,7 +123,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Yukki.skip_stream(chat_id, link, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
-        button = telegram_markup(_)
+        button = telegram_markup(_, chat_id)
         img = await gen_thumb(videoid)
         await message.reply_photo(
             photo=img,
@@ -149,7 +149,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Yukki.skip_stream(chat_id, file_path, video=status)
         except Exception:
             return await mystic.edit_text(_["call_9"])
-        button = stream_markup(_, videoid)
+        button = stream_markup(_, videoid, chat_id)
         img = await gen_thumb(videoid)
         await message.reply_photo(
             photo=img,
@@ -164,7 +164,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Yukki.skip_stream(chat_id, videoid, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
-        button = telegram_markup(_)
+        button = telegram_markup(_, chat_id)
         await message.reply_photo(
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
@@ -176,7 +176,7 @@ async def skip(cli, message: Message, _, chat_id):
         except Exception:
             return await message.reply_text(_["call_9"])
         if videoid == "telegram":
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await message.reply_photo(
                 photo=config.TELEGRAM_AUDIO_URL
                 if str(streamtype) == "audio"
@@ -187,7 +187,7 @@ async def skip(cli, message: Message, _, chat_id):
                 reply_markup=InlineKeyboardMarkup(button),
             )
         elif videoid == "soundcloud":
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             await message.reply_photo(
                 photo=config.SOUNCLOUD_IMG_URL
                 if str(streamtype) == "audio"
@@ -198,7 +198,7 @@ async def skip(cli, message: Message, _, chat_id):
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
-            button = stream_markup(_, videoid)
+            button = stream_markup(_, videoid, chat_id)
             img = await gen_thumb(videoid)
             await message.reply_photo(
                 photo=img,
